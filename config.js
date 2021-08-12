6e98b7b5dc6ef5e7dc5b66567045a751d66da9a8
Thuong1204 <88821840+Thuong1204@users.noreply.github.com>
2021-08-12 15:08:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnNo: 'Hong bao giờ',
     question:'Ỏoooooo',
     btnReply: 'Nhắn tin',
-    reply: 'Vì sao',
+    reply: 'Bởi vì yêu anh',
     mess: 'Hihi',
     messDesc: 'Nhắn tin lý do nha',
     btnAccept: 'Oke nè',
